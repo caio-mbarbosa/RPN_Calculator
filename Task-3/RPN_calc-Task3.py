e2f926768a4c7a6ed6031d06bf09707673de3c11
Caio <57820884+caio-mbarbosa@users.noreply.github.com>
2023-04-26 19:15:06 -0300

    Task 3 com regex

---
+++ Task-3/RPN_calc-Task3.py
@@ -0,0 +1,47 @@
+import re
+
+class RPNCalculator:
+    def __init__(self):
+        self.stack = []
+    
+    def calculate(self, filename):
+        with open(filename, "r") as f:
+            content = f.read()
+        
+        #A expressão regular  (\d+\.\d+|\d+|[+\-*/])extrai tokens que correspondem a números decimais, números inteiros e operadores aritméticos. Cada token é armazenado na lista tokens
+        tokens = re.findall(r'(\d+\.\d+|\d+|[+\-*/])', content)
+        token_types = []
+        
+        for token in tokens:
+            if re.match(r'\d+(\.\d+)?', token):
+                self.stack.append(float(token))
+                token_types.append(("number", float(token)))
+            elif token in "+-*/":
+                op2 = self.stack.pop()
+                op1 = self.stack.pop()
+                if token == "+":
+                    result = op1 + op2
+                elif token == "-":
+                    result = op1 - op2
+                elif token == "*":
+                    result = op1 * op2
+                elif token == "/":
+                    result = op1 / op2
+                self.stack.append(result)
+                token_types.append(("operator", token))
+            else:
+                raise ValueError("Unknown token: " + token)
+        
+        if len(self.stack) == 1:
+            result = self.stack[0]
+        else:
+            raise ValueError("Invalid expression")
+        
+        for token_type in token_types:
+            print(token_type[0], token_type[1])
+        
+        return result
+
+calculator = RPNCalculator()
+result = calculator.calculate(r'C:\Users\caio_\Downloads\Calc1.stk')
+print(result) 
